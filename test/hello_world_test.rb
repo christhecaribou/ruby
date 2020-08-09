b4f66fb3c1fd661c8d9a5e8e9e3d30feeab15362
christhecaribou <dizzyquill@protonmail.com>
2020-08-09 16:00:52 -0700

    test_ prefix needed

--- test/hello_world_test.rb
+++ test/hello_world_test.rb
@@ -3,7 +3,7 @@ require 'minitest/pride'
 require './src/hello_world'
 
 class HelloWorldTest < Minitest::Test
-  def HelloWorldTest
+  def test_hello_world
     obj = HelloWorld.new
     assert obj
   end
